8ca59d1a038ad96a99d4c4c564b92165173dc567
VICENTE ALFARO <vgalfaro1@uc.cl>
2025-12-21 15:53:33 -0300

    Cambios menores

--- prepaid_option.py
+++ prepaid_option.py
@@ -25,13 +25,16 @@ class PrepaidOption:
                 spots = holee_spots(i, j, self.shorts, forwards)
                 for k in range(j+1, self.N+1):
                     self.cred_pval[i, j] += self.cred.coup[k] * r_to_df(spots[k - j], self.dt)
-
-
+                
+                expected_free_risk_rate = (1 + 0.5 * (self.shorts.r[i+1, j] + self.shorts.r[i+1, j+1])*self.dt)**-1 
+                print(expected_free_risk_rate)
         for i in range(0,self.cred.N+1):
             self.opt_val[i,self.cred.N] = 0
 
         for j in reversed(range(self.N)):
             for i in range(j+1):
                 early = self.cred_pval[i,j] - (self.cred.nom - self.cred.sum_amort[j])
-                cont = 0.5 * (self.opt_val[i, j+1] + self.opt_val[i+1, j+1]) ## ACÁ HAY QUE MULTIPLICAR POR D[i,j]??
+                
+                expected_rate = self.shorts.r[i,j] + self.shorts.drifts[j]*self.dt
+                cont = 0.5 * (self.opt_val[i, j+1] + self.opt_val[i+1, j+1]) * r_to_df(expected_rate, self.dt)
                 self.opt_val[i,j] = max(early, cont)
